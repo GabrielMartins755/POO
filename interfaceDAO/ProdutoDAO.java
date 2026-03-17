099342134d158a40c760a7574cd288cb4720fdeb
GabrielMartins <senqueiogabriel@gmail.com>
2026-03-17 11:55:49 -0300

    feito

---
+++ interfaceDAO/ProdutoDAO.java
@@ -0,0 +1,9 @@
+package interfaceDAO;
+import java.util.List;
+
+public interface ProdutoDAO {
+    void inserir(Produto produto);
+    List<Produto> listar();
+    Produto buscarPorId(int id);
+    void remover(int id);
+}
